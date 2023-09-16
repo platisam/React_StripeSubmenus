6eb2a1e2dadf3d130b09ea161be088eff384ffc8
platisam <platisam@gmail.com>
2023-09-16 20:55:14 +0200

    creating displaySubmenu function for location

--- src/Submenu.js
+++ src/Submenu.js
@@ -1,6 +1,13 @@
 import React from "react";
+import { useGlobalContext } from "./context";
 
 const Submenu = () => {
-  return <h2>Submenu</h2>;
+  const { isSubmenuOpen } = useGlobalContext();
+
+  return (
+    <aside className={`${isSubmenuOpen ? "submenu show" : "submenu"}`}>
+      submenu
+    </aside>
+  );
 };
 export default Submenu;
